1f45afffeab307d03e36d82af272f7253042e875
Ale <tovrale3ds@gmail.com>
2019-07-06 23:15:44 -0500

    Se agregó un script de pruebas

---
+++ programa 1/ninja.py
@@ -0,0 +1,20 @@
+#my_var = 1
+#my_var = (my_var + 1)**3/8
+
+#print (my_var)
+
+#def cerradura(x,y):
+#	return x + y
+
+#print (cerradura(8,5))
+#print (type(cerradura))
+
+x = 1
+y = 3
+a = 5
+b = 7
+
+if x >= y :
+	print('x es menor que y')
+else :
+	print('x no es menor que y')
