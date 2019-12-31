29f3cc39cdd6354d045bda6597a17f48309a06e8
Ale <tovrale3ds@gmail.com>
2019-12-31 00:01:11 -0600

    Nuevo comando para actualizar clientes

--- platzi-ventas/clients/commands.py
+++ platzi-ventas/clients/commands.py
@@ -39,14 +39,51 @@ def create(ctx, name, company, email, position):
 @click.pass_context
 def list(ctx):
     """List all clients"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    click.echo('  ID  |  NAME  |  COMPANY  |  EMAIL  |  POSITION  ')
+    click.echo('*' *100)
+
+    for client in client_list:
+        click.echo('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid=client['uid'],
+            name=client['name'],
+            company=client['company'],
+            email=client['email'],
+            position=client['position']))
 
 
 @clients.command()
 @click.pass_context
 def update(ctx, client_uid):
     """Updates a single client"""
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+
+        client.echo('Client updated :) ')
+    else:
+        click.echo('Client not found :( ')
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you do not want to modift the value')
+
+    client.name = click.prompt('New name ', type=str, default=client.name)
+    client.company = click.prompt('New company ', type=str, default=client.company)
+    client.email = click.prompt('New email ', type=str, default=client.email)
+    client.position = click.prompt('New position ', type=str, default=client.position)
+
+    return client
+
+
 
 
 @clients.command()
